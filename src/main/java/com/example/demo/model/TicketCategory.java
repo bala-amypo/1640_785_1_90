ba5af82c539676d6fd8b6ad5747492be4d7e18ec
bala-amypo <bala@amypo.in>
2025-12-20 18:52:10 +0000

    User commit

--- src/main/java/com/example/demo/model/TicketCategory.java
+++ src/main/java/com/example/demo/model/TicketCategory.java
@@ -1,1 +1,49 @@
-a
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(
+    name = "ticket_categories",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "categoryName")
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+public class TicketCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String categoryName;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    
+
+    // Parameterized constructor
+    public TicketCategory(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
+
