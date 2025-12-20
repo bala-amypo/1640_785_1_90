1298e9274964d09c6223320cb34efadb9850ca2a
bala-amypo <bala@amypo.in>
2025-12-20 20:24:48 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.UserService;
+import com.example.demo.model.User;
 import java.util.List;
 public interface UserService{
 
@@ -10,5 +10,5 @@ public interface UserService{
 
     String UserDelete(int id);
     
-    User Userupdate(int id,UserService model);
+    User Userupdate(int id,User model);
 }
