274f6aadc43cc4712aaaafef52142d7b51d5e672
bala-amypo <bala@amypo.in>
2025-12-20 18:57:17 +0000

    User commit

--- src/main/java/com/example/demo/model/Ticket.java
+++ src/main/java/com/example/demo/model/Ticket.java
@@ -9,45 +9,36 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(
-    name = "ticket_categories",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "categoryName")
-    }
-)
+@Table(name = "tickets")
 @Getter
 @Setter
 @NoArgsConstructor
-public class TicketCategory {
+public class Ticket {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String categoryName;
+    @Column(nullable = false)
+    private String subject;
 
-    @Column
+    @Column(nullable = false, length = 1000)
     private String description;
+
+    @Column(nullable = false)
+    private String status = "OPEN";
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     
 
-    // Parameterized constructor
-    public TicketCategory(String categoryName, String description) {
-        this.categoryName = categoryName;
-        this.description = description;
-    }
-
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+        if (this.status == null) {
+            this.status = "OPEN";
+        }
     }
 }
-
-
-
-
 
