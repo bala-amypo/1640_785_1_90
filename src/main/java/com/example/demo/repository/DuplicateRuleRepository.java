5a8bd2937cbc205d3a0011555883b923a7e00eec
bala-amypo <bala@amypo.in>
2025-12-21 16:27:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
+++ src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.DuplicateRuleCategory;
+import com.example.demo.model.DuplicateRule;
 
 @Repository
-public interface TicketCategoryRepository
-        extends JpaRepository<TicketCategory, Long> {
+public interface DuplicateRuleRepository
+        extends JpaRepository<DuplicateRule, Long> {
 }
+DuplicateRule
