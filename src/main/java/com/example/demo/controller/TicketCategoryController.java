a21ac5bafcc2293adc54ac7427e01c8e38ff8e5a
bala-amypo <bala@amypo.in>
2025-12-21 15:21:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketCategoryController.java
+++ src/main/java/com/example/demo/controller/TicketCategoryController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-im
 
 import com.example.demo.model.TicketCategory;
 import com.example.demo.service.TicketCategoryService;
