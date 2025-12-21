2fe112101118ecddf15eafc50bbdcfc2ced98d7e
bala-amypo <bala@amypo.in>
2025-12-21 16:24:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
+++ src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
@@ -1,11 +1,10 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import com.example.demo.model.DuplicateRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.DuplicateRuleCategory;
 
-
-// public interface DuplicateRuleRepository extends JpaRepository<DuplicateRule, Long> {
-
-    
-// }
+@Repository
+public interface TicketCategoryRepository
+        extends JpaRepository<TicketCategory, Long> {
+}
--- src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
@@ -6,38 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.DuplicateRule;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
+import com.example.demo.repository.DuplicateRuleRepository;
+import com.example.demo.service.DuplicateRuleService;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class DuplicateRuleServiceImpl implements DuplicateRuleService {
 
     @Autowired
-    private UserRepository used;
+    private DuplicateRuleRepository used;
 
     @Override
-    public User registerUser(User user) {
+    public DuplicateRule registerUser3(DuplicateRule user) {
         return used.save(user);
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public List<DuplicateRule> getAllUsers3() {
         return used.findAll();
     }
 
     @Override
-    public String userDelete(Long id) {
+    public String userDelete3(Long id) {
         used.deleteById(id);
         return "Deleted successfully";
     }
 
     @Override
-    public User getUser(Long id) {
+    public DuplicateRule getUser3(Long id) {
         return used.findById(id).orElse(null);
     }
 
     @Override
-    public User userUpdate(Long id, User user) {
+    public DuplicateRule userUpdate3(Long id, DuplicateRule user) {
         if (used.existsById(id)) {
             user.setId(id);
             return used.save(user);
