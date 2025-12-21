bfe3cf3c8f3287c976cd517b1ae65eecd5d90a69
bala-amypo <bala@amypo.in>
2025-12-21 12:39:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/TicketCategoryRepository.java
+++ src/main/java/com/example/demo/repository/TicketCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.User;
 
-
+@Repository
 public interface TicketCategoryRepository extends JpaRepository<TicketCategory, Long> {
     
 }
