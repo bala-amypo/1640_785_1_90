5d491d55197e1470c4bd08cecd53b6a7df2dc494
bala-amypo <bala@amypo.in>
2025-12-21 11:42:51 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
@@ -5,70 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.Ticket;
-import com.example.demo.model.TicketCategory;
 import com.example.demo.model.User;
-import com.example.demo.repository.TicketCategoryRepository;
-import com.example.demo.repository.TicketRepository;
 import com.example.demo.repository.UserRepository;
-import com.example.demo.service.TicketService;
+import com.example.demo.service.UserService;
 
 @Service
-public class TicketServiceImpl implements TicketService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
-    private TicketRepository ticketRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private TicketCategoryRepository categoryRepository;
+    private UserRepository used;
 
     @Override
-    public Ticket createTicket(Long userId, Long categoryId, Ticket ticket) {
-
-        User user = userRepository.findById(userId).orElse(null);
-        TicketCategory category = categoryRepository.findById(categoryId).orElse(null);
-
-        if (user == null || category == null) {
-            return null;
-        }
-
-        ticket.setUser(user);
-        ticket.setCategory(category);
-
-        return ticketRepository.save(ticket);
+    public User registerUser(User user) {
+        return used.save(user);
     }
 
     @Override
-    public Ticket getTicket(Long ticketId) {
-        return ticketRepository.findById(ticketId).orElse(null);
-    }
-
-    @Override
-    public List<Ticket> getTicketsByUser(Long userId) {
-
-        User user = userRepository.findById(userId).orElse(null);
-        if (user == null) {
-            return List.of();
-        }
-
-        return ticketRepository.findByUser(user);
-    }
-
-    @Override
-    public List<Ticket> getAllTickets() {
-        return ticketRepository.findAll();
-    }
-
-     @Override
-    public User userUpdate(Long id, User user) {
-        if (used.existsById(id)) {
-            user.setId(id);
-            return used.save(user);
-        }
-        return null;
+    public List<User> getAllUsers() {
+        return used.findAll();
     }
 
     @Override
@@ -76,4 +30,19 @@ public class TicketServiceImpl implements TicketService {
         used.deleteById(id);
         return "Deleted successfully";
     }
+
+    @Override
+    public User getUser(Long id) {
+        return used.findById(id).orElse(null);
+    }
+
+    @Override
+    public User userUpdate(Long id, User user) {
+        if (used.existsById(id)) {
+            user.setId(id);
+            return used.save(user);
+        }
+        return null;
+    }
 }
+
