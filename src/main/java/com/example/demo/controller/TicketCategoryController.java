bc6aa6f87578d5c005de8915ad561b341dc16cb6
bala-amypo <bala@amypo.in>
2025-12-21 12:42:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketCategoryController.java
+++ src/main/java/com/example/demo/controller/TicketCategoryController.java
@@ -5,19 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
+import com.example.demo.model.TicketCategory;
+import com.example.demo.service.TicketCategoryService;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class TicketCategoryController {
 
     @Autowired
-    private UserService ser;
+    private TicketCategoryService ser;
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return ser.registerUser(user);
+    public TicketCategory createUser(@RequestBody TicketCategory category) {
+        return ser.registerUser(category);
     }
 
     @GetMapping
