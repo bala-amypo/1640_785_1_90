efae7fc4537c014b0e83c6837c1ab76196d9c8fe
bala-amypo <bala@amypo.in>
2025-12-21 13:56:29 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketCategoryService.java
+++ src/main/java/com/example/demo/service/TicketCategoryService.java
@@ -1,17 +1,17 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.TicketCategory;
+import java.util.List;
 
 public interface TicketCategoryService {
 
-   TicketCategory registerUser(TicketCategory user);
+    List<TicketCategory> getAll();
 
-    List<TicketCategory> getAllUsers();
+    TicketCategory getById(Long id);
 
-    TicketCategory getUser(Long id);
+    TicketCategory create(TicketCategory category);
 
-    String userDelete(Long id);
+    TicketCategory update(Long id, TicketCategory category);
 
-    TicketCategory userUpdate(Long id, TicketCategory user);
+    void delete(Long id);
 }
