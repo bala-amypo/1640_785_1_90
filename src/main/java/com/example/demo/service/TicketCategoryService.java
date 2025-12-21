19400df2094a2fbc6bd2c1ef8bff5f321cdf1aed
bala-amypo <bala@amypo.in>
2025-12-21 13:54:27 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketCategoryService.java
+++ src/main/java/com/example/demo/service/TicketCategoryService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.TicketCategory;
 
-public interface UserService {
+public interface TicketCategoryService {
 
    TicketCategory registerUser(TicketCategory user);
 
