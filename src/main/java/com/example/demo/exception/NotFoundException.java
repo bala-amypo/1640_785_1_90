c26de58ba5e2283bf3b8c08a9bcf439cfd7445d6
bala-amypo <bala@amypo.in>
2025-12-22 06:36:27 +0000

    User commit

--- src/main/java/com/example/demo/exception/NotFoundException.java
+++ src/main/java/com/example/demo/exception/NotFoundException.java
@@ -1,8 +1,8 @@
-package com.example.demo.exception;
+// package com.example.demo.exception;
 
-public class NotFoundException extends RuntimeException {
+// public class NotFoundException extends RuntimeException {
 
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
+//     public NotFoundException(String message) {
+//         super(message);
+//     }
+// }
