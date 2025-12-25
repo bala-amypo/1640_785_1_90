8c07d31f56bd48d582f81735c696d525b2662072
bala-amypo <bala@amypo.in>
2025-12-25 16:54:08 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketService.java
+++ src/main/java/com/example/demo/service/TicketService.java
@@ -1,17 +1,28 @@
+// package com.example.demo.service;
+
+// import java.util.List;
+// import com.example.demo.model.Ticket;
+
+// public interface TicketService {
+
+//     Ticket registerUser2(Ticket user);
+
+//     List<Ticket> getAllUsers2();
+
+//     Ticket getUser2(Long id);
+
+//     String userDelete2(Long id);
+
+//     Ticket userUpdate2(Long id, Ticket user);
+// }
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.Ticket;
+import java.util.List;
 
 public interface TicketService {
-
-    Ticket registerUser2(Ticket user);
-
-    List<Ticket> getAllUsers2();
-
-    Ticket getUser2(Long id);
-
-    String userDelete2(Long id);
-
-    Ticket userUpdate2(Long id, Ticket user);
+    Ticket createTicket(Long userId, Long categoryId, Ticket ticket);
+    Ticket getTicket(Long id);
+    List<Ticket> getAllTickets();
+    List<Ticket> getTicketsByUser(Long userId);
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,17 +1,27 @@
+// package com.example.demo.service;
+
+// import java.util.List;
+// import com.example.demo.model.User;
+
+// public interface UserService {
+
+//     User registerUser(User user);
+
+//     List<User> getAllUsers();
+
+//     User getUser(Long id);
+
+//     String userDelete(Long id);
+
+//     User userUpdate(Long id, User user);
+// }
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.User;
+import java.util.List;
 
 public interface UserService {
-
     User registerUser(User user);
-
-    List<User> getAllUsers();
-
     User getUser(Long id);
-
-    String userDelete(Long id);
-
-    User userUpdate(Long id, User user);
+    List<User> getAllUsers();
 }
