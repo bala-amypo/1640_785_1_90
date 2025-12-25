c935c858cbbe835d5a8ffb7d182d829e97460975
bala-amypo <bala@amypo.in>
2025-12-25 17:10:08 +0000

    User commit

--- src/main/java/com/example/demo/service/DuplicateRuleService.java
+++ src/main/java/com/example/demo/service/DuplicateRuleService.java
@@ -1,25 +1,26 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import java.util.List;
+// import java.util.List;
+// import com.example.demo.model.DuplicateRule;
+
+// public interface DuplicateRuleService {
+
+//     DuplicateRule registerUser3(DuplicateRule user);
+
+//     List<DuplicateRule> getAllUsers3();
+
+//     DuplicateRule getUser3(Long id);
+
+//     String userDelete3(Long id);
+
+//     DuplicateRule userUpdate3(Long id,DuplicateRule user);
+// }package com.example.demo.service;
+
 import com.example.demo.model.DuplicateRule;
 
 public interface DuplicateRuleService {
 
-    DuplicateRule registerUser3(DuplicateRule user);
-
-    List<DuplicateRule> getAllUsers3();
-
-    DuplicateRule getUser3(Long id);
-
-    String userDelete3(Long id);
-
-    DuplicateRule userUpdate3(Long id,DuplicateRule user);
-}
-package com.example.demo.service;
-
-import com.example.demo.model.DuplicateRule;
-
-public interface DuplicateRuleService {
     DuplicateRule createRule(DuplicateRule rule);
+
     DuplicateRule getRule(Long id);
 }
