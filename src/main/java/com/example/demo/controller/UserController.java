5ff4aa2dce1fbb3ab875ee6648a4d3d24cc632df
bala-amypo <bala@amypo.in>
2025-12-28 06:46:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -128,8 +128,5 @@ public class UserController {
     public ResponseEntity<User> getOne(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUser(id));
     }
-    if (user.getPassword() == null || user.getPassword().length() < 8) {
-    throw new IllegalArgumentException("Password must be at least 8 characters");
-}
 
 }
