3d5212bfe6d6236e29c0acd831d0ab8e6b97f393
bala-amypo <bala@amypo.in>
2025-12-29 06:36:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -47,57 +47,57 @@
 //     }
 // }
 
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import com.example.demo.model.User;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
+// import com.example.demo.model.User;
+// import com.example.demo.exception.ResourceNotFoundException;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
 
-import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import com.example.demo.model.Role;
+// import org.springframework.stereotype.Service;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import com.example.demo.model.Role;
 
-@Service
-public class UserServiceImpl implements UserService {
+// @Service
+// public class UserServiceImpl implements UserService {
  
-    private final PasswordEncoder encoder;
-    private final UserRepository userRepository;
+//     private final PasswordEncoder encoder;
+//     private final UserRepository userRepository;
    
 
-    public UserServiceImpl(UserRepository repo,PasswordEncoder encoder) {
-        this.userRepository = repo;
-        this.encoder = encoder;
-    }
+//     public UserServiceImpl(UserRepository repo,PasswordEncoder encoder) {
+//         this.userRepository = repo;
+//         this.encoder = encoder;
+//     }
 
-    @Override
-    public User register(User user) {
+//     @Override
+//     public User register(User user) {
 
-        if (user.getEmail() == null || user.getEmail().isBlank())
-            throw new IllegalArgumentException("Email cannot be empty");
+//         if (user.getEmail() == null || user.getEmail().isBlank())
+//             throw new IllegalArgumentException("Email cannot be empty");
 
-        if (user.getPassword() == null || user.getPassword().isBlank())
-            throw new IllegalArgumentException("Password cannot be empty");
+//         if (user.getPassword() == null || user.getPassword().isBlank())
+//             throw new IllegalArgumentException("Password cannot be empty");
 
    
-        user.setPassword(encoder.encode(user.getPassword()));
+//         user.setPassword(encoder.encode(user.getPassword()));
 
-       if (user.getRole() == null) {
-    user.setRole("USER");
-}
+//        if (user.getRole() == null) {
+//     user.setRole("USER");
+// }
 
-        return userRepository.save(user);
-    }
+//         return userRepository.save(user);
+//     }
 
-    @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-    }
+//     @Override
+//     public User findByEmail(String email) {
+//         return userRepository.findByEmail(email)
+//                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+//     }
 
-    @Override
-    public User findById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-    }
-}
+//     @Override
+//     public User findById(Long id) {
+//         return userRepository.findById(id)
+//                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+//     }
+// }
